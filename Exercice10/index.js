d6caac5e99faa86050dc104d13cb793636c36274
Marie-Louise Grognet <marielouise.grognet@gmail.com>
2025-04-08 11:49:18 +0200

    exercice 10 effectué

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,13 @@
 //votre code ici
 
-export default printNumbers
+export default function printNumbers (n) {
+    let result =''
+
+    for(let i=1 ; i <= n ; i++) {
+        result += i
+        if (i < n) {
+            result += ' '
+        }
+    }
+}
+
