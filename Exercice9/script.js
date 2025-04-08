5648c926f0db237a9bc2e32f856e20db1af70076
Marie-Louise Grognet <marielouise.grognet@gmail.com>
2025-04-08 11:23:26 +0200

    exercice 9 effectué

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,14 @@
 // votre code ici
 
 export default checkAge;
+
+function checkAge (int) {
+    if (int<18) {
+        return "Vous êtes mineur"
+    }
+    else if (int>=18 && int<= 65)
+        return "Vous êtes majeur"
+    else if (int>65)
+        return "Vous êtes senior"
+
+}
